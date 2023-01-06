2f41c72d15806c3a3f1db7d6a5a3eda7b8e3bdf4
ASUS <devikhirpara61@gmail.com>
2023-01-06 10:22:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:otp/homePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: OTP(),
+  ));
+}
